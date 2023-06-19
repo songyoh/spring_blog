48027345df58ffaf641a1f11c3f4618b54a1fd1e
DESKTOP-UAI0UO8\AiA <code6207@gmail.com>
2023-06-19 17:38:14 +0900

    추가 작업물 : replyUpdateTest코드 업로드

--- src/test/java/com/spring/blog/repository/ReplyRepositoryTest.java
+++ src/test/java/com/spring/blog/repository/ReplyRepositoryTest.java
@@ -2,6 +2,7 @@ package com.spring.blog.repository;
 
 import com.spring.blog.dto.ReplyFindByIdDTO;
 import com.spring.blog.dto.ReplyInsertDTO;
+import com.spring.blog.dto.ReplyUpdateDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest //@Log4j2
 public class ReplyRepositoryTest {
 
     @Autowired //test code에서는 필드 주입을 써도 무방하다
@@ -91,9 +91,30 @@ public class ReplyRepositoryTest {
 
     @Test
     @Transactional
-    @DisplayName("댓글 수정기능")
+    @DisplayName("fixture로 수정할 댓글쓴이, 댓글내용, 3번 replyId를 지정합니다. 수정 후 3번자료를 DB에서 꺼내 fixture비교 및 published_at과 updated_at이 다른지 확인")
+//    "fixture로 수정할 댓글쓴이, 댓글내용, 3번 replyId를 지정합니다.
+//    수정 후 3번자료를 DB에서 꺼내 fixture비교 및 published_at과 updated_at이 다른지 확인"
     public void updateTest(){
+        // given : 2번글의 3번째 댓글을 수정한다
+        long replyId = 3;
+        String replyWriter = "릴리수정";
+        String replyContent = "내용수정 내용수정~~";
+        ReplyUpdateDTO replyUpdateDTO = ReplyUpdateDTO.builder()
+                .replyId(replyId)
+                .replyWriter(replyWriter)
+                .replyContent(replyContent)
+                .build();
 
+        // when
+        replyRepository.update(replyUpdateDTO);
+
+        // then
+        ReplyFindByIdDTO result = replyRepository.findByReplyId(replyId);
+        assertEquals(replyWriter, result.getReplyWriter());
+        assertEquals(replyContent, result.getReplyContent());
+//        System.out.println(result);
+//        assertFalse(result.getPublishedAt().equals(result.getUpdatedAt()));
+        assertTrue(result.getUpdatedAt().isAfter(result.getPublishedAt())); //updatedAt이 publishedAt보다 이후시점(after)이다 이것이 true일 것(assertTrue)이다라고 단언
     }
 
 }
